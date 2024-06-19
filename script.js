f56e764fddec29cafb721f846046f356a5120e6f
Gaurab Paudyal <gaurab.191324@ncit.edu.np>
2024-06-19 18:03:04 +0545

    modifications of stats

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
-    .then(function(response) {
-      console.log('Email sent!', response.status, response.text);
-      alert('Email sent successfully!');
-    }, function(error) {
-      console.error('Error sending email:', error);
-      alert('Oops! Something went wrong.');
-    });
-});
+// document.getElementById('contact-form').addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
+//     .then(function(response) {
+//       console.log('Email sent!', response.status, response.text);
+//       alert('Email sent successfully!');
+//     }, function(error) {
+//       console.error('Error sending email:', error);
+//       alert('Oops! Something went wrong.');
+//     });
+// });
 
